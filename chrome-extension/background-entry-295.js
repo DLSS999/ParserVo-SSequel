a475edaafd6505a9008eac7d60d266782693a762
DLSS999 <Dmytrologvin@icloud.com>
2026-07-18 00:57:28 +0300

    Add fresh Chrome Capture 2.9.5 service worker entry

---
+++ chrome-extension/background-entry-295.js
@@ -0,0 +1,8 @@
+// ParserVo Stone Island Capture 2.9.5
+// Fresh worker filename forces Chrome to discard the 2.9.4 service worker.
+importScripts(
+  "background-stock.js",
+  "background-stop-control.js",
+  "background-loadmore-fix.js",
+  "background-transport-fix.js",
+);
