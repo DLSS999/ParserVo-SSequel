3741f22ac5885b3a1dea2cf2a58ff037689cf392
DLSS999 <Dmytrologvin@icloud.com>
2026-07-18 11:00:43 +0300

    Add extension worker entry 2.9.6

---
+++ chrome-extension/background-entry-296.js
@@ -0,0 +1,8 @@
+// ParserVo Stone Island Capture 2.9.6
+importScripts(
+  "background-stock.js",
+  "background-stop-control.js",
+  "background-loadmore-fix.js",
+  "background-transport-fix.js",
+  "background-stock-refresh.js"
+);
