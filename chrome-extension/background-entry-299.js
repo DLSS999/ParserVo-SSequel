1a1b95bd18d5c380623c73a177c160a69b04becc
DLSS999 <Dmytrologvin@icloud.com>
2026-07-18 17:59:42 +0300

    Load separate out-of-stock classification in Chrome Capture 2.9.9

---
+++ chrome-extension/background-entry-299.js
@@ -0,0 +1,11 @@
+// ParserVo Stone Island Capture 2.9.9
+importScripts(
+  "background-stock.js",
+  "background-stop-control.js",
+  "background-loadmore-fix.js",
+  "background-transport-fix.js",
+  "background-stock-refresh.js",
+  "background-size-fix.js",
+  "background-out-of-stock.js",
+  "background-resume-progress.js"
+);
