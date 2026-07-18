59e29b02583b084f8e40b603f9d15842f52ca7b3
DLSS999 <Dmytrologvin@icloud.com>
2026-07-18 13:59:40 +0300

    Add Chrome Capture 2.9.8 resumable worker entry

---
+++ chrome-extension/background-entry-298.js
@@ -0,0 +1,10 @@
+// ParserVo Stone Island Capture 2.9.8
+importScripts(
+  "background-stock.js",
+  "background-stop-control.js",
+  "background-loadmore-fix.js",
+  "background-transport-fix.js",
+  "background-stock-refresh.js",
+  "background-size-fix.js",
+  "background-resume-progress.js"
+);
