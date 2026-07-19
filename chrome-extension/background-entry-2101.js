b51d33999a877b2e86421a8c751ab79164d9d4b4
DLSS999 <Dmytrologvin@icloud.com>
2026-07-19 17:05:11 +0300

    Add Chrome Capture 2.10.1 service worker entry

---
+++ chrome-extension/background-entry-2101.js
@@ -0,0 +1,12 @@
+// ParserVo Stone Island Capture 2.10.1
+importScripts(
+  "background-stock.js",
+  "background-stop-control.js",
+  "background-loadmore-fix.js",
+  "background-transport-fix.js",
+  "background-stock-refresh.js",
+  "background-size-fix.js",
+  "background-out-of-stock.js",
+  "background-resume-progress.js",
+  "background-frame-retry.js"
+);
